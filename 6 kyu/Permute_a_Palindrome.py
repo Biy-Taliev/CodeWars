3381b531cc0286c06856fc50b21304d273599665
Biy-Taliev <113853503+Biy-Taliev@users.noreply.github.com>
2023-06-01 14:12:08 +0600

    6 kuy

---
+++ 6 kyu/Permute_a_Palindrome.py
@@ -0,0 +1,28 @@
+'''
+Write a function that will check whether ANY permutation of the characters of the input string is a palindrome. 
+Bonus points for a solution that is efficient and/or that uses only built-in language functions. 
+Deem yourself brilliant if you can come up with a version that does not use any function whatsoever.
+
+Example
+madam -> True
+adamm -> True
+junk -> False
+
+Hint
+The brute force approach would be to generate all the permutations of the string and check each one of them whether 
+it is a palindrome. However, an optimized approach will not require this at all.
+'''
+
+from collections import Counter
+
+def permute_a_palindrome(s): 
+    counter = Counter(s)
+    odd_count = 0
+
+    for count in counter.values():
+        if count % 2 != 0:
+            odd_count += 1
+            if odd_count > 1:
+                return False
+
+    return True
