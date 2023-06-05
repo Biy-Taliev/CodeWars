595cfae5735699f243269376417542703416ab97
Biy-Taliev <113853503+Biy-Taliev@users.noreply.github.com>
2023-06-05 13:52:08 +0600

    8 kuy

---
+++ 8 kyu/Beginner.py
@@ -0,0 +1,10 @@
+'''
+You will be given an array and a value . All you need to do is check whether the provided array contains the value.ax
+
+Array can contain numbers or strings. X can be either.
+
+Return if the array contains the value, if not. truefalse
+'''
+
+def check(seq, elem):
+    return elem in seq
